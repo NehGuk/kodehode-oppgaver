e4b8482cfbf2deadf921b94271636f40ca1c2e39
NehGuk <nurture.tumulus_0n@icloud.com>
2026-01-27 13:00:25 +0100

    Adds .vscode config and .prettierrc config to root

--- scripts/jan27-api-call.js
+++ scripts/jan27-api-call.js
@@ -1,12 +1,11 @@
 async function getData(url) {
-    let response = await fetch(url)
-    let data = await response.json()
-    if (!response.ok) {
-        console.error("Couldn't reach API")
-    }
-    else {
-        console.log(data)
-        return data
-    }
+  let response = await fetch(url)
+  let data = await response.json()
+  if (!response.ok) {
+    console.error("Couldn't reach API")
+  } else {
+    console.log(data)
+    return data
+  }
 }
 export { getData }
